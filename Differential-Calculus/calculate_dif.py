c2cba9a9412a0a4f77115b1af3cde1b15c6ddd01
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-03-11 21:42:56 +0700

    chore: exercise calculate derivatives

---
+++ Differential-Calculus/calculate_dif.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Turning our function into an array 
+import numpy as np
+from math import sin
+import matplotlib.pyplot as plt
+
+# our change in x value
+dx = 0.01
+def f(x):
+    return sin(x) 
+sin_x = [x for x in np.arange(0,20,dx)]
+sin_y = [f(x) for x in np.arange(0,20,dx)]
+
+plt.plot(sin_x, sin_y)
+
+sin_deriv = np.gradient(sin_y, dx)
+plt.plot(sin_x, sin_deriv)
+plt.show()
