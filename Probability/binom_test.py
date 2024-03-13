25c41fe729d8ce372cfafa1c3aeda7c6fbd22095
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-03-13 16:53:33 +0700

    chore(probability): add the study case for binom_test

--- Probability/binom_test.py
+++ Probability/binom_test.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+
+"""
+Study Cases:
+
+Out of a sample of 100 shoppers who visit a website, 23 people bought something. 
+Suppose we are running a hypothesis test for this sample with the following null and alternative hypotheses:
+
+Null hypothesis: 20% of shoppers who visit the website buy something
+
+Alternative hypothesis: More than 20% of shoppers who visit the website buy something
+
+Write a program in Python to run a one-sided binomial hypothesis test and save it 
+in a variable called p_value. 
+
+Print out p_value. What is your conclusion based on the result?
+"""
+
+
